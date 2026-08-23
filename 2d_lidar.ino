a38d3d237fd5e34cc4b8f23d0dd6b38e37d03b32
evankinsel <evankinsel25@gmail.com>
2026-08-23 01:09:10 -0400

    had to resolve sensor measurement scope and coordinate calculation

--- 2d_lidar.ino
+++ 2d_lidar.ino
@@ -1,4 +1,3 @@
-
 #include <SPI.h>
 #include <Adafruit_VL53L0X.h>
 #include <Arduino.h>
@@ -7,25 +6,25 @@
 //variable land
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
+VL53L0X_RangingMeasurementData_t measure;
+
+const int SAMPLE_SIZE = 10;
+float readings[SAMPLE_SIZE];
+
+float x;
+float y;
+
 
 
 void setup() { 
   //esp32 initialization for constant/automatic sensor reading
-    //math
-   VL53L0X_RangingMeasurementData_t measure;
-   float A = 0; // example angle for now
-   const float DEG_TO_RAD = 3.14159265f / 180.0f; 
-   float radians = A * DEG_TO_RAD; 
-   float R = measure.RangeMilliMeter; // example radius for now
-   float x = R * cos(radians);
-   float y = R * sin(radians);
-   
   
   // put your setup code here, to run once:
     // put your main code here, to run repeatedly:
-Serial.begin(115200);
-while (! Serial) { 
-  delay (1)
+  Serial.begin(115200);
+
+  while (! Serial) { 
+    delay (1);
   }
 
   lox.begin();
@@ -39,31 +38,36 @@ while (! Serial) {
 
 void loop() {
 
- Serial.println("Adafruit VL53L0X test");
-  if (!lox.begin()) {
-    Serial.println(F("Failed to boot VL53L0X"));
-    while(1);
-  }
-   VL53L0X_RangingMeasurementData_t measure;
+  Serial.println("Adafruit VL53L0X test");
 
   Serial.print("Reading a measurement... ");
+
   lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
 
   if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+    
+    Serial.print("Distance (mm): ");
+    Serial.println(measure.RangeMilliMeter);
+
+    float A = 0; // example angle for now
+    const float DEG_TO_RAD = 3.14159265f / 180.0f; 
+    float radians = A * DEG_TO_RAD; 
+    float distance = measure.RangeMilliMeter; // example radius for now
+
+    x = distance * cos(radians);
+    y = distance * sin(radians);
+
+    Serial.print("X: ");
+    Serial.println(x);
+
+    Serial.print("Y: ");
+    Serial.println(y);
+
   } else {
     Serial.println(" out of range ");
-
-    const int SAMPLE_SIZE = 10;
-    float readings[SAMPLE_SIZE];
-
-    // this is the conversion from degrees to radians
-    Serial.print(x);
-    Serial.print(",");
-    Serial.println(y);
   }
   
   {
-      delay(100);
+    delay(100);
   }
 }
