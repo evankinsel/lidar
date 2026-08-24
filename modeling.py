09722aa0cf38259b9d2dc7205eaf37784efbdb82
evankinsel <evankinsel25@gmail.com>
2026-08-24 15:58:30 -0400

    removed mesh reconstruction steps and now just visualizing what a raw point cloud may look like for now

--- modeling.py
+++ modeling.py
@@ -3,26 +3,12 @@ import pandas as pd
 import open3d as o3d
 import pymeshlab as ml
 
+#1. Load the raw XYZ coordinates
 pcd = o3d.io.read_point_cloud("pointcloud.ply")
-# 1. Load the raw XYZ coordinates
+print(len(pcd.points))
 
+#2. Downsample the cloud (Crucial for large XYZ text files)
 pcd = pcd.voxel_down_sample(voxel_size=0.05)
-# 2. Downsample the cloud (Crucial for large XYZ text files)
 
-# 3. Estimate normals (Required for XYZ data to build a 3D model)
-pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
-
-# 4. Generate the 3D surface mesh (Poisson Reconstruction)
-mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=9)
-
-# 5. Visualize the final 3D model
-o3d.visualization.draw_geometries([mesh])
-
-
-
-
-
-
-
-
-
+#3. Visualize the final 3D model in point cloud format instead of mesh for now at least
+o3d.visualization.draw_geometries([pcd], window_name="3D Model", width=800, height=600, left=50, top=50, point_show_normal=False)
